381878278ba74b66a17bbaef3355ddf0961094a5
Girish Prabhu <girishprabhu2007@gmail.com>
2026-03-21 12:33:26 +0530

    Fix password strength checker routing

--- components/tools/BuiltInToolClient.tsx
+++ components/tools/BuiltInToolClient.tsx
@@ -2,11 +2,16 @@
 
 import type { PublicContentItem } from "@/lib/content-pages";
 import UniversalToolEngineRenderer from "@/components/tools/UniversalToolEngineRenderer";
+import PasswordStrengthCheckerClient from "@/components/tools/PasswordStrengthCheckerClient";
 
 type Props = {
   item: PublicContentItem;
 };
 
 export default function BuiltInToolClient({ item }: Props) {
+  if (item.slug === "password-strength-checker") {
+    return <PasswordStrengthCheckerClient />;
+  }
+
   return <UniversalToolEngineRenderer item={item} />;
 }
