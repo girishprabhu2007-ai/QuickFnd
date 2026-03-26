ae8ca01c76bd2dc789c6711178ef72b3eb0434ff
Girish Prabhu <girishprabhu2007@gmail.com>
2026-03-26 17:18:06 +0530

    fix: force-dynamic on tool+calc pages, bust ISR cache

--- app/tools/[slug]/page.tsx
+++ app/tools/[slug]/page.tsx
@@ -34,7 +34,8 @@ type Props = {
   params: Promise<{ slug: string }>;
 };
 
-export const revalidate = 60;
+export const revalidate = 0;
+export const dynamic = "force-dynamic";
 
 function ToolUpgradeFallback({
   item,
